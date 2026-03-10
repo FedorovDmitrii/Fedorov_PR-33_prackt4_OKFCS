dbcc9b35d087f21e66d8369aa33729ff42fe6509
DmitriiFedorov <fedorovdima349@gmail.com>
2026-03-10 10:29:44 +0500

    Added Triangle tests

---
+++ Fedorov_PR-33_prackt4_OKFCS_Tests/TriangleTests.cs
@@ -0,0 +1,31 @@
+﻿using Fedorov_PR_33_prackt4_OKFCS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fedorov_PR_33_prackt4_OKFCS_Tests
+{
+    public class TriangleTests
+    {
+        [Fact]
+        public void Triangle_Area_Correct ()
+        {
+            var triangle = new Triangle(3, 4, 5);
+            Assert.Equal(6, triangle.Area( ));
+        }
+        [Fact]
+        public void Triangle_Perimeter_Correct ()
+        {
+            var triangle = new Triangle(3, 4, 5);
+            Assert.Equal(12, triangle.Perimeter( ));
+        }
+        [Fact]
+        public void Triangle_Area_Incorrect ()
+        {
+            var triangle = new Triangle(3, 4, 5);
+            Assert.Equal(10, triangle.Perimeter( ));
+        }
+    }
+}
