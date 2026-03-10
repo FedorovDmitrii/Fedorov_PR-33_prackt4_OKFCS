9152e85fec350d131ed4e3c9dd97b969577b07f9
DmitriiFedorov <fedorovdima349@gmail.com>
2026-03-10 09:11:57 +0500

    First commit

---
+++ Fedorov_PR-33_prackt4_OKFCS/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fedorov_PR_33_prackt4_OKFCS
+{
+    public class Rectangle : Shape
+    {
+        public double Width { get; }
+        public double Height { get; }
+        public override double Area ()
+        {
+            return Width * Height;
+        }
+        public override double Perimeter ()
+        {
+            return 2 * (Height + Width);
+        }
+        public override string ToString ()
+        {
+            return $"Прямоугольник: {Width}x{Height}";
+        }
+    }
+}
---
+++ Fedorov_PR-33_prackt4_OKFCS/Triangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fedorov_PR_33_prackt4_OKFCS
+{
+    public class Triangle : Shape
+    {
+        public double SideA { get; }
+        public double SideB { get; }
+        public double SideC { get; }
+        public override double Area ()
+        {
+            double p = Perimeter( ) / 2;
+            return Math.Sqrt(p * (p - SideA) * (p - SideB) * (p - SideC));
+        }
+        public override double Perimeter ()
+        {
+            return SideC + SideB + SideA;
+        }
+        public override string ToString ()
+        {
+            return $"Треушольник: {SideA}, {SideB}, {SideC}";
+        }
+    }
+}
